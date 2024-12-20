d342fbae553f483586f0c34435f5e338ca84d416
emmaegnot <bb23833@bristol.ac.uk>
2024-12-20 15:48:24 +0000

    Initial Commit

---
+++ Chelsea Helper/Chelsea_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chelsea_HelperApp.swift
+//  Chelsea Helper
+//
+//  Created by Emma Tonge on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Chelsea_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chelsea HelperTests/Chelsea_HelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Chelsea_HelperTests.swift
+//  Chelsea HelperTests
+//
+//  Created by Emma Tonge on 20/12/2024.
+//
+
+import Testing
+@testable import Chelsea_Helper
+
+struct Chelsea_HelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
